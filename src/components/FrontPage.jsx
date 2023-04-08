4c61439320f965420a00f2a755fcef58a4b28584
Harshit N Sharma <harshitpro@icloud.com>
2023-04-08 10:41:33 +0530

    Updated FrontPage

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -5,7 +5,7 @@ import  styled  from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
-  position: fixed;
+    position: fixed;
     display:flex;
     justify-content:center;
     align-items: center;
@@ -15,15 +15,18 @@ const Container = styled.div`
     top: 0px;
     background: #FFFFFF;
 `
-const Bgimg = styled.div`
+const RightSubContainer = styled.div`
   // marginLeft :
+  alignItems: flex-end;
   background-image: url(${IndustryImg});
-  width: 77%;
+  width: 65%;
   height: 100%;
   margin-left: auto;
+  background-repeat: no-repeat;
+  background-size: cover;
   top: 0px;
 `
-const Frame = styled.div`
+const LeftSubContainer = styled.div`
     display: flex;
     flex-Direction: column;
     justify-Content: flex-start;
@@ -32,7 +35,7 @@ const Frame = styled.div`
     margin-top: 15%;
     margin-left: 10%;
     height: 100vh;
-    width: 40%;
+    width: 35%;
   `
 
   const Title = styled.h1`
@@ -55,27 +58,20 @@ const Frame = styled.div`
  display: flex;
  align-items: center;
  text-align: center;
- 
- /* Grey/Tertiary */
- 
  color: #A1A0A3;
- 
- 
- /* Inside auto layout */
- 
  flex: none;
  order: 1;
  flex-grow: 0;
  `
- const Box1 = styled.div`
+ const DivTop = styled.div`
 //  display: flex;
  align-items: center;
  text-align: center;
  `
- const Box2 = styled.div`
+ const DivBottom = styled.div`
   margin-top: 120px;
  `
- const Maintxt = styled.div`
+ const Heading = styled.div`
  position: relative;
     left: 92.99px;
     top: 48.52px;
@@ -83,8 +79,9 @@ const Frame = styled.div`
     font-family: 'Inter';
     font-style: normal;
     font-weight: 800;
-    font-size: 56px;
-    line-height: 87px;
+    font-size: 3.5vw;
+    line-height: 8vh;
+    margin-right: 13%;
     display: -webkit-box;
     display: -webkit-flex;
     display: -ms-flexbox;
@@ -96,12 +93,12 @@ const Frame = styled.div`
     text-transform: uppercase;
     color: #FFFFFF;
  `
- const Cont = styled.div`
- position: absolute;
+ const Content = styled.div`
+ position: relative;
 width: 403.99px;
 height: 76px;
-left: 719.99px;
-top: 233.82px;
+left: 12%;
+top: 7%;
 
 font-family: 'Inter';
 font-style: normal;
@@ -111,36 +108,32 @@ line-height: 19px;
 display: flex;
 align-items: center;
 
-color: #FFFFFF;
- `
-const Rect1 = styled.div`
-position: absolute;
-width: 1125.01px;
-height: 571.88px;
-left: 719.99px;
-top: 356px;
+color: #FFFFFF; `
+
+const MainButton = styled.div`
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  width : 404.92px;
+  height: 73px;
+
+`
+
+const WhiteRectangle = styled.div`
+position: fixed;
+width: 51%;
+height: 53%; 
+right : 0;
+bottom: 0;
 
 background: #FFFFFF;
 border: 5px solid rgba(0, 0, 0, 0.29);
 border-radius: 31px 0px 0px 0px;
 `
-const Component6 = styled(Component3)(({ theme }) => ({
-    width: `404.92px`,
-    height: `73px`,
-    position: `absolute`,
-    left: `0px`,
-    top: `0px`,
-  }));
 
-  const Component7 = styled(Component3)(({ theme }) => ({
-    width: `404.92px`,
-    height: `73px`,
-    position: `absolute`,
-    left: `0px`,
-    top: `0px`,
-  }));
 
-  const Component8 = styled(Component3)(({ theme }) => ({
+
+  //Used component3 as a base for Button
+  const Button = styled(Component3)(({ theme }) => ({
     width: `404.92px`,
     height: `73px`,
     position: `absolute`,
@@ -151,45 +144,40 @@ const Component6 = styled(Component3)(({ theme }) => ({
   
 
 function FrontPage(props) {
-
-  const navigate = useNavigate();
-  const [level, setLevel]=useState('');
-
-
-  const navAdmin = (e) =>{
-    console.log("test");
-    console.log(e);
-     setLevel(1);
-    }
   
-    const navTeam = async () =>{
-      navigate('/formed');
-    }
-    const navEng = async () =>{
-      navigate('/formed');
-    }
+  //using react route 
+  const navigate = useNavigate();
+  
+
+  //nav function is used to navigate to Login Page by acquiring a property of level which will decide
+  // whether user is admin or engineer or team member
+  // 1 : Admin , 2: Engineer , 3: Team Member
+
+  const nav = async (myLevel) =>{
+    console.log(myLevel);
+    navigate('/login',{state : {  level: myLevel, color: 'green'  },});
+  }  
 
     return (
     <Container>
-      <Frame>
-            <Box1>
+      <LeftSubContainer>
+            <DivTop>
             <Title>
                 STP MONITORING SYSTEM
             </Title>
             <Msg>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, sit.</Msg>
-            </Box1>
-            <Box2>
-            <Component6  level="1" title="I am Admin"/>
-            <Component7  level="2" title="I am Engineer"/>
-            <Component8  level="3" title="I am team member"/>
-            </Box2>
-      </Frame>
-      <Bgimg>
-         <Maintxt> Monitor STP plant like never before </Maintxt>
-         <Cont>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quae consequatur quos praesentium distinctio  </Cont>
-         <Rect1></Rect1>
-      </Bgimg>
-      
+            </DivTop>
+            <DivBottom>
+            <MainButton onClick={ () => nav("1")}><Button title="I am Admin"/></MainButton>
+            <MainButton onClick={ () => nav("2")}><Button title="I am Engineer"/></MainButton>
+            <MainButton onClick={ () => nav("3")}><Button title="I am team member"/></MainButton>
+            </DivBottom>
+      </LeftSubContainer>
+      <RightSubContainer>
+         <Heading> Monitor STP plant like never before </Heading>
+         <Content>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quae consequatur quos praesentium distinctio  </Content>
+         <WhiteRectangle></WhiteRectangle>
+      </RightSubContainer>
     </Container>);
 }
 
